503a12096546c9971383277b12d0803a6984a7af
Jim Shaw <jimxshaw@gmail.com>
2026-09-16 17:23:59 -0500

    Add boilerplate code to load_data.py.

---
+++ data/load_data.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import pandas as pd
+
+DATA_DIR = Path("csv_files")
+TRAIN_DIR = DATA_DIR / "train"
+TEST_DIR = DATA_DIR / "test"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
