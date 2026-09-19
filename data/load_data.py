d7875b8518179a4adade5c3ad5a1abe23fd8d1f9
Jim Shaw <jimxshaw@gmail.com>
2026-09-19 09:22:33 -0500

    Add helper functions to load .csv files.

--- data/load_data.py
+++ data/load_data.py
@@ -1,12 +1,35 @@
-from pathlib import Path
+import os
 import pandas as pd
 
-DATA_DIR = Path("csv_files")
-TRAIN_DIR = DATA_DIR / "train"
-TEST_DIR = DATA_DIR / "test"
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Load the .env file where the script's configurations
+# are housed.
+load_dotenv()
+
+# Put constants here.
+DATA_DIR = Path(os.environ["DATA_DIR"])
+CSV_DIR = DATA_DIR / "csv_files"
 
 def main():
     pass
+    # DELETE THIS LATER, IT'S FOR TESTING THIS SCRIPT
+    # train_base = load_base("train")
+    # print(train_base.shape)       # Expect (1_000_000, 5)
+    # print(train_base.columns.tolist())
+
+# Returns the split directory which should 
+# be either split = "train" or "test".
+def get_split_dir(split: str) -> Path:
+    return CSV_DIR / split
+
+# Load the base table, one row per loan, for
+# the given split.
+def load_base(split: str) -> pd.DataFrame:
+    path = get_split_dir(split) / f"{split}_base.csv"
+    return pd.read_csv(path)
+
 
 if __name__ == "__main__":
     main()
