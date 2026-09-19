aaf9ca4c5485522957daf27479f9b8d75d6d3884
Jim Shaw <jimxshaw@gmail.com>
2026-09-19 09:30:15 -0500

    Add load static table function and test that it works.

--- data/load_data.py
+++ data/load_data.py
@@ -13,11 +13,8 @@ DATA_DIR = Path(os.environ["DATA_DIR"])
 CSV_DIR = DATA_DIR / "csv_files"
 
 def main():
-    pass
-    # DELETE THIS LATER, IT'S FOR TESTING THIS SCRIPT
-    # train_base = load_base("train")
-    # print(train_base.shape)       # Expect (1_000_000, 5)
-    # print(train_base.columns.tolist())
+    train_static_0 = load_static_0("train")
+    print(train_static_0)
 
 # Returns the split directory which should 
 # be either split = "train" or "test".
@@ -28,7 +25,22 @@ def get_split_dir(split: str) -> Path:
 # the given split.
 def load_base(split: str) -> pd.DataFrame:
     path = get_split_dir(split) / f"{split}_base.csv"
+
     return pd.read_csv(path)
+
+# Load and concatenate the static_0 table, which are
+# split into two files.
+def load_static_0(split: str) -> pd.DataFrame:
+    split_dir = get_split_dir(split)
+
+    part_0 = pd.read_csv(split_dir / f"{split}_static_0_0.csv")
+    part_1 = pd.read_csv(split_dir / f"{split}_static_0_1.csv")
+
+    # The ignore index parameter is needed because without it the
+    # concantenated DF will keep each part's original row index, 
+    # meaning possible duplicate index 0 for example. Using it
+    # re-numbers the index starting from 0 onward.
+    return pd.concat([part_0, part_1], ignore_index=True)
 
 
 if __name__ == "__main__":
