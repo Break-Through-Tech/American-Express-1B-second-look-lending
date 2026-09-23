0b9316769b5e2a4c990e5a625843f5acb4b4f8c4
Jim Shaw <jimxshaw@gmail.com>
2026-09-23 09:01:46 -0500

    Update join_static_cb_0 function with validate argument.

--- data/load_data.py
+++ data/load_data.py
@@ -110,7 +110,7 @@ def join_static_0(base: pd.DataFrame, static_0: pd.DataFrame) -> pd.DataFrame:
 def join_static_cb_0(base: pd.DataFrame, static_cb_0: pd.DataFrame) -> pd.DataFrame:
     # Use Left Outer Join in order to use all keys from the left dataframe
     # and missing matches from the right dataframe are filled with NaN.
-    merged = base.merge(static_cb_0, on="case_id", how="left")
+    merged = base.merge(static_cb_0, on="case_id", how="left", validate="one_to_one")
 
     # This is a row count check after merging just in case 
     # duplicate keys slipped through the cracks.
